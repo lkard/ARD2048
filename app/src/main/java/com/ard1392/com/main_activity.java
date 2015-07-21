0293b3d7a4aeb6ef80a64efae4e81466d5392bcd
LKARD <alexardelyan@gmail.com>
2015-07-21 12:29:37 +0400

    main activity add+layout

---
+++ app/src/main/java/com/ard1392/com/main_activity.java
@@ -0,0 +1,15 @@
+package com.ard1392.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.lkard.ard2048.R;
+
+public class main_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
